a76496bb38968c664dd3919633eec1878a8f2d5b
csyhuang <csyhuang@uchicago.edu>
2025-10-12 20:10:04 -0500

    update

--- ask_critic.py
+++ ask_critic.py
@@ -6,22 +6,23 @@ To run this script, you need the following API-token stored as environment varia
 """
 
 import os
+import datetime
 from llm_critic.llm_list import LargeLanguageModels
 from llm_critic.critic import critic_factory
 
 
 folder_path: str = os.environ.get("FOLDER_PATH")  # Or put in the folder path with your writing in Markdown files
-article_path: str = "ch30.md"  # A Markdown file in jekyll format
+article_path: str = os.environ.get("ARTICLE_PATH")  # A Markdown file in jekyll format
 
 # Instruction for the critic
 critic_prompt: str = \
-    "你是連載小說編輯。以下是你旗下作者的連載小說稿件。請校對並列出以下稿件的所有錯別字、不當用語、以及給予評論：\n『{content}』"
+    "你是期刊專欄編輯。以下是你旗下作者的專欄稿件。請校對並列出以下稿件的所有錯別字、不當用語、以及給予評論：\n『{content}』"
 
 # Choose your LLM critic
-critic = critic_factory(llm_model=LargeLanguageModels.GeminiFlash)
+critic = critic_factory(llm_model=LargeLanguageModels.GeminiPro)
 
 # Markdown file to output response to
-output_fname: str = f"{critic.__class__.__name__}_response_to_{article_path}"
+output_fname: str = f"{datetime.date.today().strftime("%Y%m%d")}_{critic.__class__.__name__}_response_to_{article_path}"
 
 # Generate Critique and save them into a Markdown file
 critic.generate_critique(
